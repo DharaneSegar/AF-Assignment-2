f99f183d304640a1c18963c83d5df64dc7a8634e
IT21068478 <dharanesegar08@gmail.com>
2024-05-04 09:16:40 +0530

    Modified test files

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 export default {
   testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest', // Add your existing transform for JavaScript files
+  },
+  moduleNameMapper: {
+    '\\.(mp4)$': '<rootDir>/tests/mock/fileMock.js',
+  },
 };
